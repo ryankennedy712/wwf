73e73072bef0fd7d6c27957c8034af098a3355d3
Brandon <bxwolf92@gmail.com>
2018-12-10 14:20:44 -0500

    mobile Menu

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function mobileMenu() {
+  var x = document.getElementById("navID");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
